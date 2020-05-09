b3177d152f60cb2235c8cc6e54ab7f2ae9e019a4
Nishanth4 <nishanthbalamohan4@gmail.com>
2020-05-08 22:55:09 -0400

    cleaned up to rework

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Join from './components/Join/Join';
-import Timer from './components/Timer/Timer';
+import MainTime from './components/MainTime/MainTime';
 
 const App = () => (
     <Router>
         <Route path="/" exact component={Join}/>
-        <Route path="/timer" component={Timer} />
+        <Route path="/timer" component={MainTime} />
     </Router>
 );
 
--- client/src/components/MainTime/MainTime.js
+++ client/src/components/MainTime/MainTime.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'querystring';
 import io from 'socket.io-client';
 
-import Time from '../Time/Time.js';
 import TextContainer from '../TextContainer/TextContainer.js';
 
 let socket;
@@ -14,14 +13,7 @@ const Timer = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
     const [users, setUsers] = useState('');
-    const [roomMaster, setRoomMaster] = useState('');
-    const [isClicked, setIsClicked] = useState(true);
     const ENDPOINT = 'localhost:5000';
-
-    const recievedClick = (isClicked, counter) => {//function to call in child when stop is clicked
-        setIsClicked(!isClicked);
-        console.log({'name': name, 'time': counter}); 
-    }
 
     useEffect(() =>{
         const { name, room } = queryString.parse(location.search);//gets values for name and room form url
@@ -34,47 +26,20 @@ const Timer = ({ location }) => {
         setRoom(room);
 
         socket.emit('join', { name, room }, () => {//emits join event
-
         });
 
         return () => {
             socket.emit('disconnect', room);//passes room so can be checked if empty
-
             socket.off();
         }
 
     }, [ENDPOINT, location.search]);
 
-    useEffect(() => {
-        socket.on('message', () => {
-
-        })
-
-        socket.on('new-room', (roomMasterName) => {
-            console.log('new room');
-            setRoomMaster(roomMasterName);
-            
-        })
-
-        socket.on('roomData', ({ users }) => {
-            setUsers(users);//gets all the users in the room from server and sets them
-        })
-    }, []);
-
-    useEffect(() => {
-        socket.emit('timer-clicked', {name: name, room: room});//when timer is clicked this event is sent
-    }, [isClicked])
 
     return (
         <div>
             <div>
                 <TextContainer users={users}/>
-            </div>
-            <div>
-                <Time recievedClick ={ recievedClick }/>
-            </div>
-            <div>
-                if name==roomMaster 
             </div>
         </div>
     )
